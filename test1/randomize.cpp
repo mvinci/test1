54d67c81c168575e7923c12d8bda43d07897da58
Michel Vinciguerra <michel.vinciguerra@free.fr>
2012-11-27 14:59:59 +0100

    premier commit

---
+++ test1/randomize.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Ne pas oublier d'inclure le fichier time.h
+#include <conio.h>
+
+int main(void){
+	int i = 0;
+	int nombre_aleatoire = 0;
+	for(i=0; i<50; i++){
+		nombre_aleatoire = rand()%1001;
+		printf("%d \n",nombre_aleatoire);
+	}
+	do{
+        printf("\rAppuyer sur une touche");
+      }while (kbhit()==0); // on garde la main en scrutant le clavier.
+	return 0;
+}
